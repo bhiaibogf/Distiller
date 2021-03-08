91a03304569de9f9a03aaf8e69c9c04dc98073e4
bhiaibogf <bhiaibogf@outlook.com>
2021-03-08 22:35:59 +0800

    bat

--- learning/main.py
+++ learning/main.py
@@ -1,42 +1,46 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import torch
 import torch.nn as nn
-from torch.autograd import Variable as Var
+from torch.utils.data import DataLoader, TensorDataset
 
 
 class MyModel(nn.Module):
     def __init__(self, dim):
         super(MyModel, self).__init__()
         self.__dim = dim
-        self.__w = nn.Parameter(torch.randn(dim, dtype=torch.float64))
+        self.__w = nn.Parameter(torch.randn(dim))
         self.loss_function = nn.MSELoss()
         self.optimizer = torch.optim.Adam(self.parameters(), lr=0.01)
 
     def forward(self, x):
-        inputs = torch.tensor([x ** i for i in range(self.__dim)], dtype=torch.float64)
+        inputs = torch.ones(self.__dim, x.shape[0])
+        for i in range(1, self.__dim):
+            inputs[i] = inputs[i - 1] * x
         return torch.matmul(self.__w, inputs)
 
 
 def main():
-    xs = np.arange(-3, 3, 0.05)
-    ys = np.sin(xs)
+    xs = torch.linspace(-3, 3, 1024)
+    ys = torch.sin(xs)
+
+    dataset = TensorDataset(xs, ys)
+    dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
 
     model = MyModel(5)
-    for epoch in range(600):
-        loss = Var(torch.zeros(1))
-        for xx, yy in zip(xs, ys):
+    for epoch in range(100):
+        cnt = 0
+        for xx, yy in dataloader:
+            cnt += 1
             yy_pre = model(xx)
             # print(xx, var(torch.tensor(yy)), yy_pre)
-            loss = loss + model.loss_function(yy_pre, Var(torch.tensor(yy)))
-        if epoch % 200 == 0:
-            print('loss =', loss)
+            loss = model.loss_function(yy_pre, yy)
+            model.optimizer.zero_grad()
+            loss.backward()
+            model.optimizer.step()
+            if cnt % 3 == 0:
+                print('loss =', loss)
 
-        model.optimizer.zero_grad()
-        loss.backward()
-        model.optimizer.step()
-
-    yy_pre = [model(x) for x in xs]
+    yy_pre = [model(torch.tensor([x])) for x in xs]
     plt.title('result')
     plt.plot(xs, ys)
     plt.plot(xs, yy_pre)
